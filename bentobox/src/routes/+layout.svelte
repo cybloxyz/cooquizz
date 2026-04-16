0d8e74985c2b5aa56b7fd834cc5f2123caa76228
nfnaa <tyroarrr@gmail.com>
2026-04-16 11:58:09 +0700

    solve quiz get recipe!

--- bentobox/src/routes/+layout.svelte
+++ bentobox/src/routes/+layout.svelte
@@ -5,5 +5,5 @@
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /> <title>Bentobox | solution for your tummy</title></svelte:head>
+<svelte:head><link rel="icon" href={favicon} /> <title>Cooqizz | solve quiz get recipe!</title></svelte:head>
 {@render children()}
